35f8c3a6b14430933af6c991df554ed48c33ed0c
{JosephNgugi254} <{solitaryjoe069@gmail.com}>
2025-05-26 22:26:52 +0300

    updated test book file

--- tests/test_book.py
+++ tests/test_book.py
@@ -1,10 +1,16 @@
 # tests/test_book.py
+import os
+import sys
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
 import pytest
 from sqlalchemy.orm import sessionmaker
 from lib.db.connection import Base, engine, Session
 from lib.models.book import Book
 from lib.models.author import Author
 from lib.models.genre import Genre
+from lib.models.sale import Sale
+from datetime import datetime, timezone
 
 @pytest.fixture
 def setup_database():
@@ -37,5 +43,9 @@ def test_sell_book(setup_database):
     session.add(book)
     session.commit()
     book.quantity -= 2
+    sale = Sale(book=book, quantity=2, total_price=19.98, sale_date=datetime.now(timezone.utc))
+    session.add(sale)
     session.commit()
     assert book.quantity == 3
+    assert len(book.sales) == 1
+    assert book.sales[0].quantity == 2
