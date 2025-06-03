7494a089200740d019af1340c32c3bb82e81adfb
{JosephNgugi254} <{solitaryjoe069@gmail.com}>
2025-06-03 12:32:50 +0300

    updated the sale methods

--- lib/models/sale.py
+++ lib/models/sale.py
@@ -8,10 +8,14 @@ class Sale(Base):
     __tablename__ = 'sales'
     id = Column(Integer, primary_key=True)
     book_id = Column(Integer, ForeignKey('books.id'))
+    cashier_id = Column(Integer, ForeignKey('cashiers.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
     quantity = Column(Integer, nullable=False)
     total_price = Column(Float, nullable=False)
     sale_date = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     book = relationship("Book", back_populates="sales")
+    cashier = relationship("Cashier")
+    customer = relationship("Customer")
 
     def __repr__(self):
-        return f"<Sale(book_id={self.book_id}, quantity={self.quantity}, total_price={self.total_price}, sale_date={self.sale_date})>"
+        return f"<Sale(book_id={self.book_id}, cashier_id={self.cashier_id}, customer_id={self.customer_id}, quantity={self.quantity}, total_price={self.total_price}, sale_date={self.sale_date})>"
